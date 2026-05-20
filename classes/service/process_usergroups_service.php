05c91772d555fddd4e861bee6e44c20732d59f48
Emir Kamel <p0092396@brookes.ac.uk>
2026-05-20 13:50:18 +0100

    progressing service creation

--- classes/service/process_usergroups_service.php
+++ classes/service/process_usergroups_service.php
@@ -60,33 +60,75 @@ class process_usergroups_service {
     public function processUsergroups(\progress_trace $trace, $unprocessedUsergroupsCoursesRows) : void {
         global $DB;
 
+        $courseIdNumbers = $this->buildCourseIdNumbers($unprocessedUsergroupsCoursesRows);
+        $oldCourseUserGroupEnrolmentsByCourse = $this->getOldCourseUsergroupEnrolments($trace, $courseIdNumbers);
+
+        $coursesByIdNumber = $this->getCoursesByIdNumber($courseIdNumbers);
+
         foreach ($unprocessedUsergroupsCoursesRows as $unprocessedUsergroupsCoursesRow) {
             $payload = json_decode($unprocessedUsergroupsCoursesRow->payloadjson, true);
             $courseIdNumber = $unprocessedUsergroupsCoursesRow->courseidnumber;
 
-            $course = $DB->get_record('course', ['idnumber' => $courseIdNumber]);
-            if (!$course) {
+            if (!isset($coursesByIdNumber[$courseIdNumber])) {
                 $trace->output("Course with ID number '{$courseIdNumber}' not found.");
                 continue;
             }
 
             $newCourseUserGroupEnrolments = $this->getNewCourseUsergroupEnrolments($trace, $courseIdNumber, $payload);
-            $oldCourseUserGroupEnrolments = $this->getOldCourseUsergroupEnrolments($trace, $courseIdNumber, $payload);
+            $oldCourseUserGroupEnrolments = $oldCourseUserGroupEnrolmentsByCourse[$courseIdNumber] ?? [];
 
+            $deletekeys = array_diff(array_keys($oldCourseUserGroupEnrolments), array_keys($newCourseUserGroupEnrolments));
+            $createkeys = array_diff(array_keys($newCourseUserGroupEnrolments), array_keys($oldCourseUserGroupEnrolments));
+
+            //TODO:: loop through deletes and creates and action them
         }
     }
 
+    private function getCoursesByIdNumber(array $courseIdNumbers) : array {
+        global $DB;
+
+        if (empty($courseIdNumbers)) {
+            return [];
+        }
+
+        list($insql, $params) = $DB->get_in_or_equal($courseIdNumbers, SQL_PARAMS_NAMED);
+
+        $sql = "SELECT *
+              FROM {course}
+             WHERE idnumber $insql";
+
+        $courses = $DB->get_records_sql($sql, $params);
+
+        $coursesByIdNumber = [];
+
+        foreach ($courses as $course) {
+            $coursesByIdNumber[$course->idnumber] = $course;
+        }
+
+        return $coursesByIdNumber;
+    }
+
+    private function buildCourseIdNumbers($unprocessedUsergroupsCoursesRows) : array {
+        $courseIdNumbers = [];
+
+        foreach ($unprocessedUsergroupsCoursesRows as $row) {
+            $courseIdNumbers[] = $row->courseidnumber;
+        }
+
+        return array_unique($courseIdNumbers);
+    }
+
     private function getNewCourseUsergroupEnrolments($trace, $courseIdNumber, $payload) : array {
-        $newCourseUserGroups = [];
+        $newCourseUserGroupEnrolments = [];
 
         foreach ($payload['groups'] as $group) {
             $groupName = $group['groupName'];
             $instanceName = $group['instanceName'];
 
             foreach ($group['usernames'] as $username) {
-                $key = $courseIdNumber . '|' . $groupName . '|' . $instanceName . '|' . $username;
+                $key = $this->buildUsergroupKey($courseIdNumber, $groupName, $instanceName, $username);
 
-                $newCourseUserGroups[$key] = [
+                $newCourseUserGroupEnrolments[$key] = [
                     'courseidnumber' => $courseIdNumber,
                     'groupname' => $groupName,
                     'instancename' => $instanceName,
@@ -95,6 +137,50 @@ class process_usergroups_service {
             }
         }
 
-        return $newCourseUserGroups;
+        return $newCourseUserGroupEnrolments;
+    }
+
+    private function getOldCourseUsergroupEnrolments($trace, $courseIdNumbers) : array {
+        global $DB;
+
+        if (empty($courseIdNumbers)) {
+            return [];
+        }
+
+        list($insql, $params) = $DB->get_in_or_equal($courseIdNumbers);
+
+        $sql = "SELECT *
+              FROM {local_obu_ug_sync_user}
+             WHERE courseidnumber $insql";
+
+        $rows = $DB->get_records_sql($sql, $params);
+
+        $results = [];
+
+        foreach ($rows as $row) {
+
+            $key = $this->buildUsergroupKey(
+                $row->courseidnumber,
+                $row->groupname,
+                $row->instancename,
+                $row->username
+            );
+
+            $results[$row->courseidnumber][$key] = [
+                'id' => $row->id,
+                'courseidnumber' => $row->courseidnumber,
+                'groupname' => $row->groupname,
+                'instancename' => $row->instancename,
+                'username' => $row->username,
+                'userid' => $row->userid,
+                'groupid' => $row->groupid,
+            ];
+        }
+
+        return $results;
+    }
+
+    private function buildUsergroupKey($courseIdNumber, $groupName, $instanceName, $username) : string {
+        return trim($courseIdNumber) . '|' . trim($groupName ?? '') . '|' . trim($instanceName) . '|' . trim($username);
     }
 }
